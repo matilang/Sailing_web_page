addc05a2a023874c72632e27c66adf6866bd1ab0
Mateusz Langowski <mateusz.langowski@engel.at>
2024-11-25 18:58:14 +0100

    cr

--- react/src/components/pages/CrewPage.js
+++ react/src/components/pages/CrewPage.js
@@ -1,34 +1,9 @@
 import './SectionPage.css'
 import SideHeader from '../SideHeader';
 import TitleBar from '../TitleBar';
-// import Footer from '../Footer';
-import anon from '../../images/male_icon.svg'
+import { pageLinks, pageTitle, instructorsData, racingTeamData } from './CrewPageData';
 
-const CrewPage = () => {
-  const pageTitle = 'Załoga';
-  const pageLinks = [
-    { text: 'Politechnika Gdańska', href: '/#', title: 'Wróć do poprzedniej strony' },
-    { text: 'Sekcja Żeglarska Politechniki Gdańskiej', href: '/#', title: 'Obecna strona' },
-    { text: 'Załoga', href: '/crew', title: 'Obecna strona' },
-  ];
-
-  const instructorsData = [
-    { name: 'Maria Sienkiewicz', imageSrc: anon, link: '/strona_marii' },
-    { name: 'Tomasz Goluch', imageSrc: anon, link: 'https://pg.edu.pl/p/tomasz-goluch-740294'},
-    { name: 'Jakub Szczepkowski', imageSrc: anon, link: '/strona_szczep'},
-    { name: 'Karol Pliszka', imageSrc: anon, link: '/strona_pliszka'},
-    { name: 'Michał Masiulanis', imageSrc: anon, link: '/strona/masiulanis'},
-    { name: 'Arek Kłos', imageSrc: anon, link: '/strona_klos'},
-    { name: 'Michał Czubenko', imageSrc: anon, link: 'https://pg.edu.pl/p/michal-czubenko-740072'},
-    { name: 'Szymon Swosiński', imageSrc: anon, link: 'https://pg.edu.pl/p/jakub-pankowski-3610'},
-    // Dodaj pozostałe dane instruktorów
-  ];
-
-  const racingTeamData = [
-    { name: 'Jakub Pankowski', imageSrc: anon, link: '/strona_jakuba' },
-    // Dodaj pozostałe dane członków zespołu
-  ];
-
+export default function CrewPage() {
   return (
     <div>
       <TitleBar mainTitle={pageTitle} pageLinks={pageLinks} />
@@ -54,5 +29,3 @@ const CrewPage = () => {
     </div>
   );
 };
-
-export default CrewPage;
---
+++ react/src/components/pages/CrewPageData.js
@@ -0,0 +1,24 @@
+import anon from '../../images/male_icon.svg'
+
+export const pageTitle = 'Załoga';
+
+export const pageLinks = [
+    { text: 'Politechnika Gdańska', href: '/#', title: 'Wróć do poprzedniej strony' },
+    { text: 'Sekcja Żeglarska Politechniki Gdańskiej', href: '/#', title: 'Obecna strona' },
+    { text: 'Załoga', href: '/crew', title: 'Obecna strona' },
+  ];
+
+export const instructorsData = [
+    { name: 'Maria Sienkiewicz', imageSrc: anon, link: '/strona_marii' },
+    { name: 'Tomasz Goluch', imageSrc: anon, link: 'https://pg.edu.pl/p/tomasz-goluch-740294'},
+    { name: 'Jakub Szczepkowski', imageSrc: anon, link: '/strona_szczep'},
+    { name: 'Karol Pliszka', imageSrc: anon, link: '/strona_pliszka'},
+    { name: 'Michał Masiulanis', imageSrc: anon, link: '/strona/masiulanis'},
+    { name: 'Arek Kłos', imageSrc: anon, link: '/strona_klos'},
+    { name: 'Michał Czubenko', imageSrc: anon, link: 'https://pg.edu.pl/p/michal-czubenko-740072'},
+    { name: 'Szymon Swosiński', imageSrc: anon, link: 'strona_szym'},
+  ];
+
+export const racingTeamData = [
+    { name: 'Jakub Pankowski', imageSrc: anon, link: '/https://pg.edu.pl/p/jakub-pankowski-3610' },
+  ];
